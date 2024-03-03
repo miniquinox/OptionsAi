d004714772035c4d958c89e9c5ca1b163a7db4a8
Joaquin Carretero <quinocarreteromartinez@gmail.com>
2024-03-03 12:00:29 -0800

    Initial Commit

---
+++ OptionsAi/OptionsAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptionsAiApp.swift
+//  OptionsAi
+//
+//  Created by Quino on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct OptionsAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
